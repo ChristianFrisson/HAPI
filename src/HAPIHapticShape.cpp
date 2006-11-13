afc9d7f374b7c112d2c47c294d6a341011c7e6d2
daniel <daniel@f896c5a5-350d-0410-967e-bc877d8f5295>
2006-11-13 08:39:48 +0000

    Implemented genShapeId and delShapeId
    git-svn-id: https://www.h3dapi.org:8090/HAPI/trunk@38 f896c5a5-350d-0410-967e-bc877d8f5295

---
+++ src/HAPIHapticShape.cpp
@@ -0,0 +1,50 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file HAPISurfaceObject.cpp
+/// \brief cpp file for HAPISurfaceObject
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "HAPIHapticShape.h"
+
+using namespace HAPI;
+
+int HAPIHapticShape::current_max_id = 0;
+list< int > HAPIHapticShape::free_ids;
+
+int HAPIHapticShape::genShapeId() {
+  if( free_ids.empty() ) {
+    return current_max_id++;
+  } else {
+    int id = free_ids.front();
+    free_ids.pop_front();
+    return id;
+  }
+}
+
+void HAPIHapticShape::delShapeId( int id ) {
+  // todo: remove all resources used by id, e.g. hl shape id
+  free_ids.push_front( id );
+}
