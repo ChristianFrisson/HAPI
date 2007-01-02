5324645287f2e613998630e6ea1f3f0ccdc1a62b
daniel <daniel@f896c5a5-350d-0410-967e-bc877d8f5295>
2007-01-02 13:35:59 +0000

    Some updates to manual
    git-svn-id: https://www.h3dapi.org:8090/HAPI/trunk@57 f896c5a5-350d-0410-967e-bc877d8f5295

---
+++ doc/examples/constant_force.cpp
@@ -0,0 +1,36 @@
+
+#include <AnyHapticsDevice.h>
+#include <HapticForceField.h>
+
+using namespace HAPI;
+
+int main(int argc, char* argv[]) {
+  // The force to render
+  Vec3 force_to_render = Vec3( 1, 0, 0 );
+
+  // Create a new haptics device, using any device connected.
+  auto_ptr< AnyHapticsDevice > device( new AnyHapticsDevice );
+
+  // initialize the device
+  if( device->initDevice() != HAPIHapticsDevice::SUCCESS ) {
+    // initilization failed, print error message and quit
+    cerr << device->getLastErrorMsg() << endl;
+    return 1;
+  }
+  
+  // enable the device(forces and positions will be updated)
+  device->enableDevice();
+
+  // add the force effect to render
+  device->addEffect( new HapticForceField( force_to_render ) );
+  
+  // transfer the effect to the haptics loop.
+  device->transferObjects();
+
+  // wait for keyboard press, then finish program 
+  while(true) {
+    
+  } 
+  
+  return 0;
+}
