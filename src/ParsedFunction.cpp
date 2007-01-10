08673c4e7daec7568d419cca9374f2dd4cae8d04
daniel <daniel@f896c5a5-350d-0410-967e-bc877d8f5295>
2007-01-10 13:53:36 +0000

    Added function objects
    git-svn-id: https://www.h3dapi.org:8090/HAPI/trunk@71 f896c5a5-350d-0410-967e-bc877d8f5295

---
+++ src/ParsedFunction.cpp
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file ParsedFunction.cpp
+/// \brief cpp file for ParseFunction.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "ParsedFunction.h" 
+
+// H3DUtil include
+#include <Console.h>
+
+using namespace HAPI;
+
+#ifdef _MSC_VER
+#pragma comment( lib, "fparser.lib" )
+#endif
+
+bool ParsedFunction::setFunctionString( const string &function,
+                                        const string &evaluation_params ) {
+  function_string = function;
+  params_string = evaluation_params;
+  int res = fparser.Parse( function_string, evaluation_params );
+  if(res < 0) {
+    have_valid_function = true;
+    return true;
+  }
+ 
+  H3DUtil::Console(3) << fparser.ErrorMsg() << endl;
+  have_valid_function = false;
+  return false;
+}
+
+/// Evaluate the function. 
+/// input points to the input values to the function.
+HAPIFloat ParsedFunction::evaluate( HAPIFloat *input ) {
+  if( have_valid_function )
+    return fparser.Eval( input );
+  else return 0;
+}
