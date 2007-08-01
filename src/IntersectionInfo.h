e2dcff2585220d3df8518697a4328fe27edd0624
markus <markus@f896c5a5-350d-0410-967e-bc877d8f5295>
2007-08-01 09:49:19 +0000

    Added files I of course forgot to add in the previous commit. Used for correct deletion of hapihapticshape.
    git-svn-id: https://www.h3dapi.org:8090/HAPI/trunk@180 f896c5a5-350d-0410-967e-bc877d8f5295

---
+++ src/IntersectionInfo.h
@@ -0,0 +1,86 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+// TODO: 
+// Base code that this code was built upon was contributed by .....
+//
+//
+/// \file CollisionStructures.h
+/// \brief Header file for CollisionStructures, 
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __INTERSECTIONINFO_H__
+#define __INTERSECTIONINFO_H__
+#include <HAPITypes.h>
+
+namespace HAPI {
+
+  namespace Bounds {
+
+    //class GeometryPrimitive;
+     typedef enum {
+        BACK,
+        FRONT,
+        FRONT_AND_BACK
+      } FaceType;
+
+    /// \brief The IntersectionInfo struct contains information about an 
+    /// intersection.
+    struct IntersectionInfo {
+     
+
+      /// Constructor.
+      IntersectionInfo( const Vec3 &_point = Vec3(), 
+                        const Vec3 &_normal = Vec3(),
+                        const Vec3 &_tex_coord = Vec3(),
+                        FaceType intersected_face = FRONT,
+                        int _id = -1 ) :
+      point( _point ),
+      normal( _normal ),
+      tex_coord( _tex_coord ),
+      id( _id ) { intersection = false; }
+
+      /// The intersection point.
+      Vec3 point;
+
+      /// The normal at the intersection point.
+      Vec3 normal;
+
+      /// The texture coordinate at the intersection point.
+      Vec3 tex_coord;
+
+      /// The face that was intersected. BACK or FRONT.
+      FaceType face;
+
+      bool intersection;
+
+      /// Gradient describing the change of tex coord depending on
+      /// change in pos at the intersection point.
+      Matrix3 pos_to_tex_coord_gradient;
+      
+      /// \brief The id of the primitive that was intersected if applicable,
+      /// e.g. triangle index. -1 if no id exists.
+      int id;
+    };
+  }
+}
+#endif
---
+++ src/PlaneConstraint.cpp
@@ -0,0 +1,70 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file Scene.cpp
+/// \brief CPP file for Scene, X3D scene-graph node
+///
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <PlaneConstraint.h>
+
+using namespace HAPI;
+using namespace Bounds;
+
+bool PlaneConstraint::lineIntersect( const Vec3 &from, 
+                                     const Vec3 &to,
+                                     Bounds::IntersectionInfo &result ) {
+      Vec3 from_to = to - from;
+      if( normal * from_to > Constants::epsilon ) { 
+        /*if( result.normal.z == 0.5 ) {
+          cerr << "a" << endl;
+          cerr << normal * from_to << endl;
+          }*/
+        return false;
+      }
+
+
+      HAPIFloat denom = normal * from_to;
+      if( denom * denom < Constants::epsilon ) {
+        /*        if( result.normal.z == 0.5 )
+                  cerr << "b" << endl; */
+        return false;
+      }
+      HAPIFloat u = ( normal * ( point - from ) ) / denom; 
+      /*      if( result.normal.z == 0.5 )
+              cerr << u << endl;*/
+      if( u <= 1 + Constants::epsilon && u >= 0 - Constants::epsilon ) {
+
+        if( u < 0 ) u = 0;
+        if( u > 1 ) u = 1;
+                //cerr << u << endl;
+        result.point = from + u * from_to;
+        result.normal = normal;
+        // TODO: temp. Mybe use derivatives to get correct tex coord
+        result.tex_coord = tex_coord;
+        return true;
+      }
+      return false;
+}
