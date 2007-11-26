e537c713a2ec2e76c079f0fac2c55073b0d831a2
markus <markus@f896c5a5-350d-0410-967e-bc877d8f5295>
2007-11-26 15:43:57 +0000

    Renamed and fixed HaptikHapticsDevice. Should work now but due to some problems with openhaptics not even Haptiks own examples work.
    git-svn-id: https://www.h3dapi.org:8090/HAPI/trunk@264 f896c5a5-350d-0410-967e-bc877d8f5295

---
+++ include/HAPI/HaptikHapticsDevice.h
@@ -0,0 +1,99 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file HaptikHapticsDevice.h
+/// \brief Header file for HaptikHapticsDevice.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef __HAPI_HAPTIKHAPTICSDEVICE_H__
+#define __HAPI_HAPTIKHAPTICSDEVICE_H__
+
+#include <HAPI/HAPIHapticsDevice.h>
+
+#ifdef HAVE_HAPTIK_LIBRARY
+#include <RSLib/Haptik.hpp>
+
+namespace HAPI {
+
+  /// \class HaptikHapticsDevice
+  /// Base class for all haptic devices. 
+  class HAPI_API HaptikHapticsDevice: public HAPIHapticsDevice {
+  public:
+
+    /// Constructor.
+    HaptikHapticsDevice(): 
+      haptik_device( NULL ) {
+      changeHaptikDevice( HAPTIK_DEFAULT_DEVICE );
+    }
+
+    /// Destructor. Stops haptics rendering and remove callback functions.
+    virtual ~HaptikHapticsDevice() {
+      disableDevice();
+      releaseDevice();
+    }
+
+    void changeHaptikDevice( UINT32 device_id );
+
+    inline int getHaptikIndex() {
+      int dev_index = -1;
+      if( haptik_device ) {
+        RSLib::HaptikDeviceInfo info;
+        haptik_device->GetInfo( info );
+        for(UINT32 i = 0 ; i< haptik.numberOfDevices ; i++) {
+          if( haptik.device[i].id == info.id ) {
+            dev_index = i;
+            break;
+          }
+        }
+      }
+      return dev_index;
+    }
+
+  protected:
+
+    /// Initialize the haptics device.
+    /// \param _thread_frequency is the desired haptic frequency.
+    virtual bool initHapticsDevice( int _thread_frequency = 1000 );
+
+    /// Release all resources allocated to the haptics device.
+    virtual bool releaseHapticsDevice();
+
+    virtual void updateDeviceValues( DeviceValues &dv,
+                                     HAPITime dt );
+
+    virtual void sendOutput( DeviceOutput &dv,
+                             HAPITime dt );
+
+    RSLib::Haptik haptik;
+    /// The device currently in use.
+    RSLib::IHaptikDeviceInterface haptik_device;
+
+    /// Lock when changing device.
+    H3DUtil::MutexLock change_haptik_device_lock;
+  };
+}
+
+#endif
+#endif
---
+++ src/HaptikHapticsDevice.cpp
@@ -0,0 +1,123 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file HaptikHapticsDevice.cpp
+/// \brief Cpp file for HaptikHapticsDevice.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+#include <HAPI/HaptikHapticsDevice.h>
+
+#ifdef HAVE_HAPTIK_LIBRARY
+
+using namespace HAPI;
+
+void HaptikHapticsDevice::changeHaptikDevice( UINT32 device_id ) {
+  if( haptik_device ) {
+    RSLib::HaptikDeviceInfo info;
+    haptik_device->GetInfo( info );
+    int index = -1;
+
+    // find the index of the current device
+    for(UINT32 i = 0 ; i<haptik.numberOfDevices ; i++) {
+      if( haptik.device[i].id == info.id ) {
+        index = i;
+        break;
+      }
+    }
+
+    assert( index != -1 );
+
+    // if the new device_id is different than the current device
+    // disable the old one and initialize the new one.
+    if( device_id != info.id ) {
+      haptik_device->Release();
+      change_haptik_device_lock.lock();
+      haptik_device = 
+        (RSLib::IHaptikDeviceInterface) 
+        haptik.GetDeviceInterface( device_id );
+      change_haptik_device_lock.unlock();
+    }
+  } else {
+    change_haptik_device_lock.lock();
+    haptik_device = 
+      (RSLib::IHaptikDeviceInterface) haptik.GetDeviceInterface( device_id );
+    change_haptik_device_lock.unlock();
+  }
+}
+
+bool HaptikHapticsDevice::initHapticsDevice( int _thread_frequency ) {
+  bool return_value = false;
+  if( haptik_device ) {
+    return_value = haptik_device->Init() == HAPTIK_SUCCESS;
+    if( !return_value )
+      haptik_device->Release();
+  }
+  return return_value;
+}
+
+bool HaptikHapticsDevice::releaseHapticsDevice() {
+  if( haptik_device ) {
+    haptik_device->Release();
+    return true;
+  }
+  return false;
+}
+
+void HaptikHapticsDevice::updateDeviceValues( DeviceValues &dv,
+                                       HAPITime dt ) {
+  change_haptik_device_lock.lock();
+  HAPIHapticsDevice::updateDeviceValues( dv, dt );
+  if( haptik_device ) {
+    RSLib::HaptikData data;
+    haptik_device->Read( data );
+    dv.position = Vec3( data.position.x, data.position.y, data.position.z );
+    dv.velocity = Vec3( data.velocity.x, data.velocity.y, data.velocity.z );
+    dv.button_status = data.buttonMask;
+    dv.orientation = Rotation(
+      Matrix3( data.matrix.r0.x, data.matrix.r1.x, data.matrix.r2.x,
+               data.matrix.r0.y, data.matrix.r1.y, data.matrix.r2.y,
+               data.matrix.r0.z, data.matrix.r1.z, data.matrix.r2.z ) );
+  }
+  change_haptik_device_lock.unlock();
+}
+
+void HaptikHapticsDevice::sendOutput( DeviceOutput &dv,
+                               HAPITime dt ) {
+  change_haptik_device_lock.lock();
+  if( haptik_device ) {
+    RSLib::HaptikData data;
+    data.forceFeedback.x = (float)dv.force.x;
+    data.forceFeedback.y = (float)dv.force.y;
+    data.forceFeedback.z = (float)dv.force.z;
+    data.torqueFeedback.x = (float)dv.torque.x;
+    data.torqueFeedback.y = (float)dv.torque.y;
+    data.torqueFeedback.z = (float)dv.torque.z;
+    haptik_device->Write( data );
+  }
+  change_haptik_device_lock.unlock();
+}
+
+#endif
