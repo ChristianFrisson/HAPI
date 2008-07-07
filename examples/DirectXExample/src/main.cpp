bce6cad403f1143bc4c9eb3574ef94ab5cd40625
markus <markus@f896c5a5-350d-0410-967e-bc877d8f5295>
2008-07-07 07:52:38 +0000

    Changed copyright message to talk about HAPI instead of H3D API. Also fixed a bunch of line endings.
    git-svn-id: https://www.h3d.org:8090/HAPI/trunk@393 f896c5a5-350d-0410-967e-bc877d8f5295

--- examples/DirectXExample/src/main.cpp
+++ examples/DirectXExample/src/main.cpp
@@ -14,7 +14,7 @@
 //    GNU General Public License for more details.
 //
 //    You should have received a copy of the GNU General Public License
-//    along with H3D API; if not, write to the Free Software
+//    along with HAPI; if not, write to the Free Software
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 //    A commercial license is also available. Please contact us at 
--- include/HAPI/HapticPointSet.h
+++ include/HAPI/HapticPointSet.h
@@ -1,20 +1,20 @@
 //////////////////////////////////////////////////////////////////////////////
 //    Copyright 2004-2007, SenseGraphics AB
 //
-//    This file is part of H3D API.
+//    This file is part of HAPI.
 //
-//    H3D API is free software; you can redistribute it and/or modify
+//    HAPI is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation; either version 2 of the License, or
 //    (at your option) any later version.
 //
-//    H3D API is distributed in the hope that it will be useful,
+//    HAPI is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU General Public License for more details.
 //
 //    You should have received a copy of the GNU General Public License
-//    along with H3D API; if not, write to the Free Software
+//    along with HAPI; if not, write to the Free Software
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 //    A commercial license is also available. Please contact us at 
