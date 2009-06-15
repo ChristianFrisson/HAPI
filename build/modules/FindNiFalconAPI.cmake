29774c28a236340593ca0c99d7c053a2afcdb677
daniel <daniel@f896c5a5-350d-0410-967e-bc877d8f5295>
2009-06-15 13:25:27 +0000

    Added support for falcon using the libnifalcon library. Patch provided by KJ
    git-svn-id: https://www.h3dapi.org:8090/HAPI/trunk@473 f896c5a5-350d-0410-967e-bc877d8f5295

---
+++ build/modules/FindNiFalconAPI.cmake
@@ -0,0 +1,110 @@
+# - Find NiFalconAPI
+# Find the native NIFALCONAPI headers and libraries.
+#
+#  NIFALCONAPI_INCLUDE_DIR -  where to find hdl.h, etc.
+#  NIFALCONAPI_LIBRARIES    - List of libraries when using NiFalconAPI.
+#  NIFALCONAPI_FOUND        - True if NiFalconAPI found.
+
+
+# Look for the header file.
+FIND_PATH(NIFALCONAPI_INCLUDE_DIR
+  NAMES
+  falcon/comm/FalconCommLibFTDI.h
+  falcon/firmware/FalconFirmwareNovintSDK.h
+  falcon/kinematic/FalconKinematicStamper.h
+  falcon/util/FalconFirmwareBinaryNvent.h
+  falcon/core/FalconDevice.h
+  PATHS $ENV{FALCON_SUPPORT}/include
+  "/Program Files/NiFalcon/include"
+  $ENV{NOVINT_DEVICE_SUPPORT}/include )
+MARK_AS_ADVANCED(NIFALCONAPI_INCLUDE_DIR)
+
+# Look for the ftdi library.
+FIND_LIBRARY(NIFALCON_COMM_FTDI_LIBRARY
+  NAMES nifalcon_comm_libftdi
+  PATHS $ENV{FALCON_SUPPORT}/lib
+  "/Program Files/NiFalcon/lib"
+  $ENV{NOVINT_DEVICE_SUPPORT}/lib)
+MARK_AS_ADVANCED(NIFALCON_COMM_FTDI_LIBRARY)
+
+# Look for the usb library.
+FIND_LIBRARY(NIFALCON_COMM_USB_LIBRARY
+  NAMES nifalcon_comm_libusb
+  PATHS $ENV{FALCON_SUPPORT}/lib
+  "/Program Files/NiFalcon/lib"
+  $ENV{NOVINT_DEVICE_SUPPORT}/lib)
+MARK_AS_ADVANCED(NIFALCON_COMM_USB_LIBRARY)
+
+# Look for the ftd2xx library.
+FIND_LIBRARY(NIFALCON_COMM_FTD2XX_LIBRARY
+  NAMES nifalcon_comm_ftd2xx
+  PATHS $ENV{FALCON_SUPPORT}/lib
+  "/Program Files/NiFalcon/lib"
+  $ENV{NOVINT_DEVICE_SUPPORT}/lib)
+MARK_AS_ADVANCED(NIFALCON_COMM_FTD2XX_LIBRARY)
+
+# Set to true if we have any communication library supported
+SET( HAVE_NI_FALCON_COMM_LIBRARY NIFALCON_COMM_FTD2XX_LIBRARY OR NIFALCON_COMM_USB_LIBRARY OR NIFALCON_COMM_FTDI_LIBRARY)
+
+# Look for the nifalcon_cpp library.
+FIND_LIBRARY(NIFALCON_LIBRARY
+  NAMES nifalcon
+  PATHS $ENV{FALCON_SUPPORT}/lib
+  "/Program Files/NiFalcon/lib"
+  $ENV{NOVINT_DEVICE_SUPPORT}/lib)
+MARK_AS_ADVANCED(NIFALCON_LIBRARY)
+
+
+# Copy the results to the output variables.
+IF(NIFALCONAPI_INCLUDE_DIR AND HAVE_NI_FALCON_COMM_LIBRARY AND NIFALCON_LIBRARY)
+  SET(NIFALCONAPI_FOUND 1)
+
+  # add the nifalcon library
+  SET(NIFALCONAPI_LIBRARIES ${NIFALCON_LIBRARY} )
+
+  # comm libraries, preferred order usb, ftdi, ftd2xx
+  IF( NIFALCON_COMM_USB_LIBRARY )
+     OPTION(NIFALCON_LIBUSB
+           "Use libusb for communication with Falcon device"
+           ON)
+    SET( HAVE_SET_OPTION 1 )
+    SET(NIFALCONAPI_LIBRARIES ${NIFALCONAPI_LIBRARIES} ${NIFALCON_COMM_USB_LIBRARY} )
+  ENDIF( NIFALCON_COMM_USB_LIBRARY )
+  
+  # ftdi library 
+  IF( NIFALCON_COMM_FTDI_LIBRARY )
+    IF( DEFINED HAVE_SET_OPTION )
+      OPTION(NIFALCON_LIBFTDI
+             "Use libftdi for communication with Falcon device"
+             OFF)
+    ELSE( DEFINED HAVE_SET_OPTION )
+      OPTION(NIFALCON_LIBFTDI
+             "Use libftdi for communication with Falcon device"
+             ON)
+    ENDIF( DEFINED HAVE_SET_OPTION )
+	     
+    SET( HAVE_SET_OPTION 1 )
+    SET(NIFALCONAPI_LIBRARIES ${NIFALCONAPI_LIBRARIES} ${NIFALCON_COMM_FTDI_LIBRARY} )
+  ENDIF( NIFALCON_COMM_FTDI_LIBRARY )
+
+  # ftd2xx library
+  IF( NIFALCON_COMM_FTD2XX_LIBRARY )
+    IF( DEFINED HAVE_SET_OPTION )
+      OPTION(NIFALCON_LIBFTD2XX
+             "Use libftd2xx for communication with Falcon device"
+    	     OFF )
+    ELSE( DEFINED HAVE_SET_OPTION )
+      OPTION(NIFALCON_LIBFTD2XX
+             "Use libftd2xx for communication with Falcon device"
+    	     ON )
+    ENDIF( DEFINED HAVE_SET_OPTION )
+
+    SET(NIFALCONAPI_LIBRARIES ${NIFALCONAPI_LIBRARIES} ${NIFALCON_COMM_FTD2XX_LIBRARY} )
+  ENDIF( NIFALCON_COMM_FTD2XX_LIBRARY )
+    
+  SET(NIFALCONAPI_INCLUDE_DIR ${NIFALCONAPI_INCLUDE_DIR})
+ELSE(NIFALCONAPI_INCLUDE_DIR AND HAVE_NI_FALCON_COMM_LIBRARY AND NIFALCON_LIBRARY)
+  SET(NIFALCONAPI_FOUND 0)
+  SET(NIFALCONAPI_LIBRARIES)
+  SET(NIFALCONAPI_INCLUDE_DIR)
+ENDIF(NIFALCONAPI_INCLUDE_DIR  AND HAVE_NI_FALCON_COMM_LIBRARY AND NIFALCON_LIBRARY)
---
+++ src/NiFalconHapticsDevice.cpp
@@ -0,0 +1,215 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//    Copyright 2008-2009, Kyle Machulis
+//    Copyright 2009, Karljohan Lundin Palmerius
+//
+//    This file is part of HAPI.
+//
+//    HAPI is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    HAPI is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with HAPI; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file NiFalconHapticsDevice.cpp
+/// \brief Cpp file for NiFalconHapticsDevice.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+#include <HAPI/NiFalconHapticsDevice.h>
+
+#ifdef HAVE_NIFALCONAPI
+
+#ifdef NIFALCON_LIBFTD2XX
+# include <falcon/comm/FalconCommFTD2XX.h>
+#endif
+
+#ifdef NIFALCON_LIBFTDI
+# include <falcon/comm/FalconCommLibFTDI.h>
+#endif
+
+#ifdef NIFALCON_LIBUSB
+# include <falcon/comm/FalconCommLibUSB.h>
+#endif
+
+#include <falcon/firmware/FalconFirmwareNovintSDK.h>
+#include <falcon/kinematic/FalconKinematicStamper.h>
+#include <falcon/util/FalconFirmwareBinaryNvent.h>
+
+#include <falcon/grip/FalconGripFourButton.h>
+
+#include <H3DUtil/DynamicLibrary.h>
+
+
+using namespace HAPI;
+using namespace libnifalcon;
+
+
+namespace NiFalconHapticsDeviceInternal {
+  list< string > nifalcon_device_libs;
+}
+
+HAPIHapticsDevice::HapticsDeviceRegistration 
+NiFalconHapticsDevice::device_registration
+( "NiFalconHapticsDevice",
+  &(newInstance< NiFalconHapticsDevice >),
+  NiFalconHapticsDeviceInternal::nifalcon_device_libs );
+
+bool NiFalconHapticsDevice::initHapticsDevice( int _thread_frequency ) {
+  
+#ifdef NIFALCON_LIBUSB
+  H3DUtil::DynamicLibrary::load("libnifalcon_comm_libusb.so");
+  device.setFalconComm<libnifalcon::FalconCommLibUSB>();
+#else
+#ifdef NIFALCON_LIBFTD2XX
+  device.setFalconComm<libnifalcon::FalconCommLibFTD2XX>();
+#else
+#ifdef NIFALCON_LIBFTDI
+  device.setFalconComm<libnifalcon::FalconCommLibFTDI>();
+#endif
+#endif
+#endif
+  
+  device.setFalconFirmware<libnifalcon::FalconFirmwareNovintSDK>();
+  device.setFalconKinematic<libnifalcon::FalconKinematicStamper>();
+  device.setFalconGrip<libnifalcon::FalconGripFourButton>();
+ 
+  unsigned int num_falcons;	
+  if(!device.getDeviceCount(num_falcons)) {
+    std::cout << "Cannot get device count" << std::endl;
+  }
+	
+  std::cout << "Falcons found: " << num_falcons << std::endl;
+  
+  std::cout << "Opening falcon" << std::endl;
+  
+  if(!device.open(0)) {
+    std::cout << "Cannot open falcon - Error: " << device.getErrorCode() << std::endl;
+    return false;
+  }
+
+  std::cout << "Opened falcon" << std::endl;
+
+  bool has_firmware = false;
+  for(int i = 0; i < 10; ++i) {
+    if(!device.getFalconFirmware()->loadFirmware(true, NOVINT_FALCON_NVENT_FIRMWARE_SIZE, const_cast<uint8_t*>(NOVINT_FALCON_NVENT_FIRMWARE)))
+      {
+	std::cout << "Cannot load firmware" << std::endl;
+      }
+    else
+      {
+	has_firmware = true;
+	std::cout <<"firmware loaded" << std::endl;
+	break;
+      }
+  }
+  if(!has_firmware) return false;
+
+
+  // set up a communication thread for the device.
+  com_thread = 
+    new H3DUtil::PeriodicThread( H3DUtil::ThreadBase::HIGH_PRIORITY, 1000 );
+  com_thread->setThreadName( "NiFalconHapticsDevice com thread" );
+  
+  com_func_cb_handle = com_thread->asynchronousCallback( com_func, this );
+
+  return true;
+}
+
+bool NiFalconHapticsDevice::releaseHapticsDevice() {
+  HAPIHapticsDevice::disableDevice();
+  
+  if( com_thread ) {
+    if( com_func_cb_handle != -1 ) {
+      com_thread->removeAsynchronousCallback( com_func_cb_handle );
+      com_func_cb_handle = -1;
+    }
+    delete com_thread;
+    com_thread = NULL;
+  }
+  
+  return true;
+}
+
+void NiFalconHapticsDevice::updateDeviceValues( DeviceValues &dv,
+                                                HAPITime dt ) {
+  HAPIHapticsDevice::updateDeviceValues( dv, dt );
+  com_lock.lock();
+  dv.position = current_values.position;
+  calculateVelocity(dv, dt);
+  dv.orientation = current_values.orientation;
+  dv.button_status = current_values.button_status;
+  com_lock.unlock();
+}
+
+void NiFalconHapticsDevice::sendOutput( DeviceOutput &dv,
+                                        HAPITime dt ) {
+  com_lock.lock();
+  current_values.force = dv.force;
+  current_values.torque = dv.torque;
+  com_lock.unlock();
+}
+
+H3DUtil::PeriodicThread::CallbackCode
+NiFalconHapticsDevice::com_func( void *data ) {
+  NiFalconHapticsDevice *hd = 
+    static_cast< NiFalconHapticsDevice * >( data );
+  
+  if( hd->getDeviceState() != HAPIHapticsDevice::ENABLED ) {
+    return H3DUtil::PeriodicThread::CALLBACK_CONTINUE; }
+  
+  hd->device.getFalconFirmware()->setHomingMode( true );
+  hd->device.runIOLoop();
+  if( hd->device.getFalconFirmware()->isHomed() ) {
+
+    // get position
+    boost::array<double,3> pos = hd->device.getPosition();
+    Vec3 position( pos[0] , pos[1] , (pos[2] - .150f) );
+    
+    HAPIInt32 button_status;
+    
+    // get buttons
+    boost::shared_ptr<FalconGrip> grip = hd->device.getFalconGrip();
+    for( int i = 0 ; i < grip->getNumDigitalInputs() ; i++ ){
+      if( grip->getDigitalInput(i) ){
+        button_status |= 0x01 << i; }
+    }
+    
+    Rotation orientation;
+    
+    // transfer values to/from haptics thread.
+    hd->com_lock.lock();
+    
+    hd->current_values.position = position;
+    hd->current_values.button_status = button_status;
+    hd->current_values.orientation = orientation;
+
+    boost::array<double,3> f;
+    f[0] = hd->current_values.force.x;
+    f[1] = hd->current_values.force.y;
+    f[2] = hd->current_values.force.z;
+     
+    hd->com_lock.unlock();
+ 
+    // send force
+    hd->device.setForce(f);
+ }
+  
+  return H3DUtil::PeriodicThread::CALLBACK_CONTINUE;
+}
+
+#endif
