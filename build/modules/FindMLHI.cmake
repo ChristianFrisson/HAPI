0a99019a555642be91271df4d957dee617478c7b
markus <markus@f896c5a5-350d-0410-967e-bc877d8f5295>
2010-08-26 09:05:23 +0000

    Added preliminary support for MLHIHapticsdevices, that is, device support for devices from Butterfly Haptics. Note that I am not even sure it compiles correctly. Patch supplied in bug report 0000242 and modified by me to fit with the other HAPI files.
    git-svn-id: https://www.h3dapi.org:8090/HAPI/trunk@558 f896c5a5-350d-0410-967e-bc877d8f5295

---
+++ build/modules/FindMLHI.cmake
@@ -0,0 +1,42 @@
+# - Find MLHI
+# Find the MLHI library
+#
+#  MLHI_INCLUDE_DIR -  where to find MLHI/ml_api.h
+#  MLHI_LIBRARIES    - List of libraries when using MLHI
+#  MLHI_FOUND        - True if MLHI found
+
+GET_FILENAME_COMPONENT(module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH )
+
+# Look for the header file.
+FIND_PATH(MLHI_INCLUDE_DIR NAMES MLHI/ml_api.h
+                                PATHS /usr/local/include/
+                                DOC "Path in which the file MLHI/ml_api.h is located." )
+MARK_AS_ADVANCED(MLHI_INCLUDE_DIR)
+
+# Look for the library.
+FIND_LIBRARY(MLHI_LIBRARY NAMES mlhi_api_linux
+                        PATHS /usr/local/lib/
+                        DOC "Path to MLHI library." )
+MARK_AS_ADVANCED(MLHI_LIBRARY)
+
+# Copy the results to the output variables.
+IF(MLHI_INCLUDE_DIR AND MLHI_LIBRARY)
+  SET(MLHI_FOUND 1)
+  SET(MLHI_LIBRARIES ${MLHI_LIBRARY} )
+  SET(MLHI_INCLUDE_DIR ${MLHI_INCLUDE_DIR})
+ELSE(MLHI_INCLUDE_DIR AND MLHI_LIBRARY)
+  SET(MLHI_FOUND 0)
+  SET(MLHI_LIBRARIES)
+  SET(MLHI_INCLUDE_DIR)
+ENDIF(MLHI_INCLUDE_DIR  AND MLHI_LIBRARY)
+
+# Report the results.
+IF(NOT MLHI_FOUND)
+  SET(MLHI_DIR_MESSAGE
+    "The MLHI API was not found. Make sure to set MLHI_LIBRARY and MLHI_INCLUDE_DIR. If you do not have the MLHI library you will not be able to use the MLHI devices.")
+  IF(MLHI_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "${MLHI_DIR_MESSAGE}")
+  ELSEIF(NOT MLHI_FIND_QUIETLY)
+    MESSAGE(STATUS "${MLHI_DIR_MESSAGE}")
+  ENDIF(MLHI_FIND_REQUIRED)
+ENDIF(NOT MLHI_FOUND)
---
+++ include/HAPI/MLHIHapticsDevice.h
@@ -0,0 +1,127 @@
+//////////////////////////////////////////////////////////////////////////////
+//    This file is part of HAPI.
+//
+//    HAPI is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    HAPI is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with HAPI; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file MLHIHapticsDevice.h
+/// \brief Header file for MLHIHapticsDevice
+/// Code contributed by Sebastian Bozlee and Jennifer McFatridge
+/// University of Hawaii at Manoa Human-Robot Interface Laboratory and then
+/// adapted to the HAPI code structure.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef __MLHIHAPTICSDEVICE_H__
+#define __MLHIHAPTICSDEVICE_H__
+
+#include <HAPI/HAPIHapticsDevice.h>
+
+#ifdef HAVE_MLHI
+
+#include <MLHI/ml_api.h>
+#include <pthread.h>
+
+namespace HAPI {
+
+  /// \ingroup HapticsDevices
+  /// \class MLHIHapticsDevice
+  /// \brief Interface to MLHI haptics devices.
+  /// That is, devices from Butterfly haptics. Note that this
+  /// is preliminary support. Untested by SenseGraphics.
+  class HAPI_API MLHIHapticsDevice : public HAPIHapticsDevice {
+  public:
+    /// Constructor
+    MLHIHapticsDevice(string _server_ip_addr = "" )
+      : device_hdl(NULL), current_position(Vec3(0,0,0)),
+      current_rotation(Rotation()), velocity(Vec3(0,0,0)),
+      server_ip_addr(_server_ip_addr)
+    {
+      // No need for mutex yet.
+      for (int i = 0; i < 6; i++)
+        forces.values[i] = 0;
+    }
+
+    virtual ~MLHIHapticsDevice() {}
+
+    /// Initializes haptics device.
+    /// \param _thread_frequency ignored. Always 1000.
+    bool initHapticsDevice(int _thread_frequency = 1000);
+
+    /// Releases resources allocated by initHapticsDevice.
+    bool releaseHapticsDevice();
+
+    /// Turns rotation gains on (not allowed) or low (allowed).
+    /// Being able to rotate makes the device less stable.
+    void allowRotation(bool allow);
+
+    /// Registers the device so that it may be used with the AnyHapticsClass
+    static HapticsDeviceRegistration device_registration;
+
+  protected:
+    /// Updates device information for the rest of the program
+    void updateDeviceValues(DeviceValues &dv, HAPITime dt);
+
+    /// Sets forces to send to maglev device
+    void sendOutput(DeviceOutput &dv, HAPITime dt);
+
+  private:
+    int connect(ml_device_handle_t * device_hdl);
+    int checkFault(ml_device_handle_t device_hdl);
+
+    // returns false on failure
+    bool takeoffWhenReady();
+    void defyGravity();
+    void waitForButtonPress();
+    void initGains();
+    void inline storeInDevices();
+    void inline removeFromDevices();
+    void inline registerCallbacks();
+    void inline unregisterCallbacks();
+    void renderForces(ml_position_t position);
+    void updateButtonStatus(int _button_status);
+    void updatePositionAndVelocity(ml_position_t position);
+
+    static inline MLHIHapticsDevice* getDeviceByHandle(ml_device_handle_t device_hdl);
+
+    ml_device_handle_t device_hdl;
+    ml_forces_t forces;
+    ml_gain_vec_t initial_gains;*/
+    H3DUtil::MutexLock forces_lock;
+    H3DUtil::MutexLock position_and_velocity_lock;
+    Vec3 current_position;
+    Rotation current_rotation;
+    Vec3 velocity;
+    int button_status;
+    string server_ip_addr;
+
+    static map<ml_device_handle_t, MLHIHapticsDevice*> devices;
+
+    static int tickCallback(ml_device_handle_t device_hdl,
+                            ml_position_t* position);
+    static int checkFaultCallback(ml_device_handle_t device_hdl,
+                                  ml_fault_t fault);
+    static int buttonCallback(ml_device_handle_t device_hdl,
+                              ml_button_t button);
+  };
+
+}
+
+#endif
+
+#endif
