0c3677d51fc93ccff3f70ce311b2e0d55dca970c
daniel <daniel@f896c5a5-350d-0410-967e-bc877d8f5295>
2011-01-26 15:57:45 +0000

    Updates to EntactHapticsDevice. Fixed a startup bug, added more error checking forces, torques, position and orientation is now transformed to correct coord system
    git-svn-id: https://www.h3dapi.org:8090/HAPI/trunk@573 f896c5a5-350d-0410-967e-bc877d8f5295

--- src/EntactHapticsDevice.cpp
+++ src/EntactHapticsDevice.cpp
@@ -79,7 +79,7 @@ EntactHapticsDevice::~EntactHapticsDevice() {
 bool EntactHapticsDevice::initHapticsDevice( int _thread_frequency ) {
   
   if( !EAPI_initialized ) {
-    int res = openEAPI( &nr_entact_devices, 25 );
+    int res = openEAPI( &nr_entact_devices, 1 );
     if( res != EAPI_OK ) {
       stringstream s;
       s << "Warning: Failed to open Entact device. Unable to query for devices." << endl;
@@ -124,16 +124,36 @@ bool EntactHapticsDevice::initHapticsDevice( int _thread_frequency ) {
     }
   }
 
-  if( needsCalibration() ) calibrateDevice();
-  setModeEAPI( device_id, EAPI_MODE_FC );
-  enableDeviceEAPI( device_id );
-  
+  if( needsCalibration() ) {
+    if( !calibrateDevice() ) {
+      stringstream s;
+      s << "Warning:  Failed to open Entact device. Homing failed. ";
+      setErrorMsg( s.str() );
+      return false;
+    }
+  }
+
+  if( setModeEAPI( device_id, EAPI_MODE_FC ) != EAPI_OK ) {
+    stringstream s;
+    s << "Warning:  Failed to open Entact device.  Failed to change to force control mode.";
+    setErrorMsg( s.str() );
+    return false;
+  }
+    
+  if( enableDeviceEAPI( device_id ) !=  EAPI_OK ) {
+    stringstream s;
+    s << "Warning:  Failed to open Entact device.  Failed to enable device.";
+    setErrorMsg( s.str() );
+    return false;
+  }
+    
+#ifdef USE_COM_THREAD
   com_thread = 
     new H3DUtil::PeriodicThread( H3DUtil::ThreadBase::HIGH_PRIORITY, 1000 );
   com_thread->setThreadName( "Entact device com thread" );
 
   com_func_cb_handle = com_thread->asynchronousCallback( com_func, this );
-
+#endif
   return true;
 }
 
@@ -161,11 +181,18 @@ void EntactHapticsDevice::updateDeviceValues( DeviceValues &dv,
   HAPIHapticsDevice::updateDeviceValues( dv, dt );
 
   if( device_id != -1 ) {
+#ifdef USE_COM_THREAD
     com_lock.lock();
+#else
+    com_func( this );
+#endif
+   
     dv.position = current_values.position;
     dv.orientation = current_values.orientation;
     dv.button_status = current_values.button_status;
+#ifdef USE_COM_THREAD
     com_lock.unlock();
+#endif
   }
 }
 
@@ -181,36 +208,55 @@ void EntactHapticsDevice::sendOutput( DeviceOutput &dv,
 
 H3DUtil::PeriodicThread::CallbackCode
 EntactHapticsDevice::com_func( void *data ) {
+
   EntactHapticsDevice *hd = 
     static_cast< EntactHapticsDevice * >( data );
   
   if( hd->device_id != -1 ) {
-    double positions[6]; 
+    double positions[12]; 
     readTaskPositionEAPI( hd->device_id, positions );
-    double velocity[6];
-    // TODO:
+
+    //double velocity[6];
+    // TODO: velocity
+
     //readTaskVelocityEAPI( hd->device_id, velocity );
 
-    Rotation orientation = 
-      Rotation( 1, 0, 0,  positions[5] ) * 
-      Rotation( 0, 1, 0,  positions[4] ) * 
-      Rotation( 0, 0, 1, -positions[3] );
+    Matrix3 orientationMatrix ( positions[3], positions[4], positions[5],
+				positions[6], positions[7], positions[8],
+				positions[9], positions[10], positions[11] );
+    
+    Rotation orientation( orientationMatrix );
 
+    // rotation from entact device space to h3d space
+    Rotation offset = Rotation(1,0,0, H3DUtil::Constants::pi ) * Rotation( 0, 1, 0, H3DUtil::Constants::pi/2 );
+
+    // rotate the axis of orientation to match h3d space 
+    orientation.axis =  offset * orientation.axis; 
+
+    // the rotation from entact is based on a vector along negative y-axis 
+    // (positive y in H3D space). In H3D it is based on positive z, so first
+    // rotate positive z axis to positive y before applying orientation.
+    orientation = orientation * Rotation( 1, 0, 0, -H3DUtil::Constants::pi/2 );
+
+#ifdef USE_COM_THREAD
     hd->com_lock.lock();
+#endif
 
-    hd->current_values.position = Vec3( -positions[0], positions[2], positions[1] );
-    hd->current_values.velocity = Vec3( -velocity[0], velocity[2], velocity[1] );
+    hd->current_values.position = Vec3( positions[2], -positions[1], positions[0] - 210 ) * 1e-3;
+    //hd->current_values.velocity = Vec3( velocity[2], velocity[2], velocity[1] );
     //hd->current_values.button_status = button;
     hd->current_values.orientation = orientation;
- 
-    Vec3 force = hd->current_values.force;
+    Vec3 force = hd->current_values.force * 1000;
     Vec3 torque = hd->current_values.torque;
-    hd->com_lock.unlock();
 
-    double forces[6] = { -force[0], force[2], force[1], -torque[0], torque[2], torque[1] };
-    
+#ifdef USE_COM_THREAD
+    hd->com_lock.unlock();
+#endif
+
+    double forces[5] = { force[2], -force[1], force[0], torque[1], -torque[0] };
     writeForceEAPI( hd->device_id, forces );
   }
+
   return H3DUtil::PeriodicThread::CALLBACK_CONTINUE;
 }
 
@@ -222,14 +268,14 @@ bool EntactHapticsDevice::calibrateDevice() {
 /// Called to query a device of its calibration status. 
 bool EntactHapticsDevice::needsCalibration() {
   int res = isHomedEAPI( device_id );
-  return res == EAPI_OK;
+  return res != EAPI_OK;
 }
 
 /// Returns the number of Entact devices that are currently
 /// connected to the computer.
 int EntactHapticsDevice::getNumberConnectedEntactDevices() {
  if( !EAPI_initialized ) {
-   int res = openEAPI( &nr_entact_devices, 25 );
+   int res = openEAPI( &nr_entact_devices, 1 );
    if( res == EAPI_OK ) {
      EAPI_initialized = true;
    }
