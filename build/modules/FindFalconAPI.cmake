c2b9f38142d7187122e406cf14544f39c98dadb6
markus <markus@f896c5a5-350d-0410-967e-bc877d8f5295>
2012-11-13 14:39:16 +0000

    Fixes to FindFalconAPI.cmake to not try to find HDAL SDK for 64 bit build since it will not work anyways.
    git-svn-id: https://www.h3dapi.org:8090/HAPI/trunk@630 f896c5a5-350d-0410-967e-bc877d8f5295

--- build/modules/FindFalconAPI.cmake
+++ build/modules/FindFalconAPI.cmake
@@ -5,23 +5,36 @@
 #  FALCONAPI_LIBRARIES    - List of libraries when using FalconAPI.
 #  FALCONAPI_FOUND        - True if FalconAPI found.
 
+SET( program_files_path "$ENV{ProgramW6432}" )
+IF( NOT program_files_path )
+  SET( program_files_path "$ENV{ProgramFiles}" )
+ENDIF( NOT program_files_path )
+
+SET( FALCON_INCLUDE_SEARCH_PATH "" )
+SET( FALCON_LIB_SEARCH_PATH "" )
+IF( WIN32 )
+  IF( NOT CMAKE_CL_64 )
+    SET( FALCON_INCLUDE_SEARCH_PATH $ENV{NOVINT_FALCON_SUPPORT}/include
+                                    ${program_files_path}/Novint/Falcon/HDAL/include
+                                    ${program_files_path}/Novint/HDAL_SDK_2.1.3/include
+                                    $ENV{NOVINT_DEVICE_SUPPORT}/include )
+    SET( FALCON_LIB_SEARCH_PATH $ENV{NOVINT_FALCON_SUPPORT}/lib
+                                ${program_files_path}/Novint/Falcon/HDAL/lib
+                                ${program_files_path}/Novint/HDAL_SDK_2.1.3/lib
+                                $ENV{NOVINT_DEVICE_SUPPORT}/lib )
+  ENDIF( NOT CMAKE_CL_64 )
+ENDIF( WIN32 )
 
 # Look for the header file.
 FIND_PATH(FALCONAPI_INCLUDE_DIR NAMES hdl/hdl.h 
-                                PATHS $ENV{NOVINT_FALCON_SUPPORT}/include
-                                      $ENV{PROGRAMFILES}/Novint/Falcon/HDAL/include
-                                      $ENV{PROGRAMFILES}/Novint/HDAL_SDK_2.1.3/include
-                                      $ENV{NOVINT_DEVICE_SUPPORT}/include
+                                PATHS ${FALCON_INCLUDE_SEARCH_PATH}
                                 DOC "Path in which the file hdl/hdl.h is located. File is part of HDAL SDK." )
 
 MARK_AS_ADVANCED(FALCONAPI_INCLUDE_DIR)
 
 # Look for the library.
 FIND_LIBRARY(FALCONAPI_HDL_LIBRARY NAMES hdl 
-                        PATHS $ENV{NOVINT_FALCON_SUPPORT}/lib
-                              $ENV{PROGRAMFILES}//Novint/Falcon/HDAL/lib
-                              $ENV{PROGRAMFILES}//Novint/HDAL_SDK_2.1.3/lib
-                              $ENV{NOVINT_DEVICE_SUPPORT}/lib
+                        PATHS ${FALCON_LIB_SEARCH_PATH}
                         DOC "Path to hdl library. Library is part of HDAL SDK." )
 MARK_AS_ADVANCED(FALCONAPI_HDL_LIBRARY)
 
@@ -40,6 +53,9 @@ ENDIF(FALCONAPI_INCLUDE_DIR  AND FALCONAPI_HDL_LIBRARY)
 IF(NOT FALCONAPI_FOUND)
   SET(FALCONAPI_DIR_MESSAGE
     "The Novint Falcon API(HDAL SDK) was not found. Make sure to set FALCONAPI_HDL_LIBRARY and FALCONAPI_INCLUDE_DIR. If you do not have it you will not be able to use the Novint Falcon Haptics device.")
+  IF( CMAKE_CL_64 )
+    SET( FALCONAPI_DIR_MESSAGE "${FALCONAPI_DIR_MESSAGE} NOVINT HAS NOT RELEASED A 64 BIT VERSION OF HDAL SDK YET." )
+  ENDIF( CMAKE_CL_64 )
   IF(FalconAPI_FIND_REQUIRED)
     MESSAGE(FATAL_ERROR "${FALCONAPI_DIR_MESSAGE}")
   ELSEIF(NOT FalconAPI_FIND_QUIETLY)
