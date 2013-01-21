c64c2c62f97af8569ace8f49a32662e767f818f7
markus <markus@f896c5a5-350d-0410-967e-bc877d8f5295>
2013-01-21 07:58:48 +0000

    Updated year in copyright notice in each file.
    git-svn-id: https://www.h3d.org:8090/HAPI/trunk@651 f896c5a5-350d-0410-967e-bc877d8f5295

--- examples/ThreadExamples/SimpleThreadPrintLock/SimpleThreadPrintLock.cpp
+++ examples/ThreadExamples/SimpleThreadPrintLock/SimpleThreadPrintLock.cpp
@@ -1,3 +1,32 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2008-2013, SenseGraphics AB
+//
+//    This file is part of HAPI.
+//
+//    HAPI is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    HAPI is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with HAPI; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file SimpleThreadPrintLock.cpp
+/// \brief CPP file which provides example code to show H3DUtil::SimpleThread
+/// with locking.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
 #include <H3DUtil/Threads.h>
 
 // To be able to use std functions.
--- src/DeviceLog.cpp
+++ src/DeviceLog.cpp
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////////////
-//    Copyright 2004-2009, SenseGraphics AB
+//    Copyright 2004-2013, SenseGraphics AB
 //
 //    This file is part of HAPI.
 //
