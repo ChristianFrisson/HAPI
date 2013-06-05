376eca023d588ce4f6edf4c148ddc57f846bd5ac
(no author) <(no author)@f896c5a5-350d-0410-967e-bc877d8f5295>
2013-06-05 15:07:04 +0000

    Added PlaybackHapticsDevice to play back recordings made with the DeviceLog force effect.
    git-svn-id: https://www.h3d.org:8090/HAPI/trunk@708 f896c5a5-350d-0410-967e-bc877d8f5295

---
+++ include/HAPI/PlaybackHapticsDevice.h
@@ -0,0 +1,165 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2013, SenseGraphics AB
+//
+//    This file is part of HAPI.
+//
+//    HAPI is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    HAPI is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with HAPI; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file PlaybackHapticsDevice.h
+/// \brief Header file for PlaybackHapticsDevice.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef __HAPI_PLAYBACKHAPTICSDEVICE_H__
+#define __HAPI_PLAYBACKHAPTICSDEVICE_H__
+
+#include <HAPI/HAPIHapticsDevice.h>
+
+namespace HAPI {
+
+  class HAPI_API PlaybackHapticsDevice: public HAPIHapticsDevice {
+  public:
+
+    /// Constructor.
+    PlaybackHapticsDevice () :
+      data_start_pos ( 0 ),
+      playback_time ( 0 ),
+      playing ( false ),
+      playback_start_time ( 0 ),
+      seek_to_time ( 0 ),
+      playback_speed ( 1 ),
+      binary ( false ) {
+    }
+
+    virtual void updateDeviceValues( DeviceValues &dv, HAPI::HAPITime dt );
+
+    /// Output is ignored on a fake haptics device.
+    virtual void sendOutput( DeviceOutput &dv,
+            HAPI::HAPITime dt ) {}
+
+    /// Implementation of initHapticsDevice.
+    virtual bool initHapticsDevice( int _thread_frequency = 1000 ) {
+      return true;
+    }
+
+    /// Releases all resources allocated in initHapticsDevice. 
+    virtual bool releaseHapticsDevice() { return true; }
+
+    /// Specify the filename to use to read playback data from.
+    /// 
+    /// The file format is the same format that is produced by the DeviceLog
+    /// force effect and is described in more detail there. For synchronized 
+    /// playback the file should conatian the TIME column. The following columns
+    /// are played back, if present: RAW_POSITION, RAW_ORIENTATION, RAW_VELOCITY,
+    /// BUTTONS. All other columns are ignored.
+    ///
+    /// \param _filename The filename of the file containing the playback data
+    /// \param _binary   If true then the file is considered to contain binary data
+    ///                  otherwise it is considered to contain text data
+    ///
+    /// \return True if the file was opened successfully for reading
+    bool loadRecording ( const std::string& _filename, bool _binary= false );
+
+    void closeRecording ();
+
+    /// Start playing back the loaded recording from the current playback position
+    void startPlayback ();
+
+    /// Stop playing back the recording. 
+    ///
+    /// All device outputs are frozen at there last values and
+    /// playback can be resumed by calling startPlayback()
+    void stopPlayback ();
+
+    /// Move playback position to the specified point in time. 
+    ///
+    /// This function only takes effect the next time playback is started.
+    ///
+    /// \param _time The time in the recording to move to
+    void seekToTime ( HAPITime _time );
+
+    /// Set the speed of the playback.
+    ///
+    /// This function only takes effect the next time playback is started.
+    ///
+    /// \param _speed The desired speed of the playback, where 1 is the 
+    ///               original recording speed.
+    void setPlaybackSpeed ( HAPIFloat _speed );
+
+    /// Returns the time of the last event that was played back
+    HAPITime getPlaybackTime ();
+
+    /// Returns true if currently playing back events. False if idle, or
+    /// the playback has completed.
+    bool isPlaying ();
+
+  protected:
+    typedef std::vector < std::string > StringList;
+
+    /// Read the next entry in the recording
+    ///
+    /// \param[out] _dv   The device values read from the recording
+    /// \param[out] _time The time of the device values in the recording
+    ///
+    bool getPlaybackValuesNext ( HAPI::HAPIHapticsDevice::DeviceValues& _dv, HAPI::HAPITime& _time );
+
+    /// Read the next entry in the recording that corresponds to the specified time
+    ///
+    /// Entries are read from the recording until the time stamp of an entry is >= _time
+    ///
+    /// \param[out] _dv   The device values read from the recording
+    /// \param[in] _time  The time to search forward for
+    ///
+    bool getPlaybackValuesAtTime ( HAPI::HAPIHapticsDevice::DeviceValues& _dv, HAPI::HAPITime _time );
+
+    bool getPlaybackValuesNextBinary ( HAPI::HAPIHapticsDevice::DeviceValues& _dv, HAPI::HAPITime& _time );
+
+    bool getPlaybackValuesNextText ( HAPI::HAPIHapticsDevice::DeviceValues& _dv, HAPI::HAPITime& _time );
+
+    StringList readColumnNamesBinary ();
+
+    StringList readColumnNamesText ();
+
+    std::ifstream playback_file;
+
+    std::streampos data_start_pos;
+
+    StringList field_names;
+
+    HAPITime playback_time;
+
+    HAPIHapticsDevice::DeviceValues playback_device_values;
+
+    bool playing;
+
+    HAPITime playback_start_time;
+
+    HAPITime seek_to_time;
+
+    HAPITime playback_speed;
+
+    bool binary;
+
+    /// A mutex lock used to access playback data
+    H3DUtil::MutexLock playback_lock;
+  };
+}
+
+#endif
---
+++ src/PlaybackHapticsDevice.cpp
@@ -0,0 +1,318 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2013, SenseGraphics AB
+//
+//    This file is part of HAPI.
+//
+//    HAPI is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    HAPI is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with HAPI; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file PlaybackHapticsDevice.cpp
+/// \brief Cpp file for PlaybackHapticsDevice.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+#include <HAPI/PlaybackHapticsDevice.h>
+
+using namespace HAPI;
+
+void PlaybackHapticsDevice::updateDeviceValues( DeviceValues &dv, HAPI::HAPITime dt ) {
+  HAPIHapticsDevice::updateDeviceValues( dv, dt );
+
+  playback_lock.lock();
+  if ( playing ) {
+    if ( !getPlaybackValuesAtTime ( dv, playback_speed * (H3DUtil::TimeStamp()-playback_start_time) ) ) {
+      // end of playback or error
+      playing= false;
+    }
+  } else {
+    dv= playback_device_values;
+  }
+  playback_lock.unlock();
+}
+
+PlaybackHapticsDevice::StringList PlaybackHapticsDevice::readColumnNamesBinary () {
+  StringList fields;
+
+  int c= playback_file.get();
+  while ( c != '\0' ) {
+    std::string field_name;
+    while ( c != '\0' ) {
+      field_name+= c;
+      c= playback_file.get();
+    }
+    fields.push_back ( field_name );
+    c= playback_file.get();
+  }
+
+  return fields;
+}
+
+PlaybackHapticsDevice::StringList PlaybackHapticsDevice::readColumnNamesText () {
+  StringList fields;
+
+  std::string line;
+  std::getline ( playback_file, line );
+
+  istringstream iss(line);
+  while ( iss ) {
+    std::string field_name;
+    iss >> field_name;
+    if ( !field_name.empty() ) {
+      fields.push_back ( field_name );
+    }
+  }
+
+  return fields;
+}
+
+bool PlaybackHapticsDevice::loadRecording ( const std::string& _filename, bool _binary ) {
+  playback_lock.lock();
+  
+  binary= _binary;
+  playback_file= ifstream ( _filename.c_str(), std::ios::binary );
+  if ( !playback_file.good() ) {
+    return false;
+  }
+
+  // Read the column names
+  if ( binary ) {
+    field_names= readColumnNamesBinary();
+  } else {
+    field_names= readColumnNamesText();
+  }
+  data_start_pos= playback_file.tellg();
+  playback_time= 0;
+
+  playback_lock.unlock();
+  
+  return true;
+}
+
+void PlaybackHapticsDevice::closeRecording () {
+  playback_lock.lock();
+
+  playing= false;
+  playback_file.close();
+
+  playback_lock.unlock();
+}
+
+void PlaybackHapticsDevice::startPlayback () {
+  playback_lock.lock();
+
+  if ( !playing ) {
+    if ( seek_to_time >= 0 ) {
+      if ( seek_to_time < playback_time || playback_time < 0 ) {
+        playback_file.clear();
+        playback_file.seekg ( data_start_pos );
+      }
+      playback_time= seek_to_time;
+      seek_to_time= -1;
+    }
+
+    playback_start_time= H3DUtil::TimeStamp()-playback_time;
+    playing= true;
+  }
+
+  playback_lock.unlock();
+}
+
+void PlaybackHapticsDevice::stopPlayback () {
+  playback_lock.lock();
+
+  if ( playing ) {
+    playing= false;
+  }
+
+  playback_lock.unlock();
+}
+
+void PlaybackHapticsDevice::seekToTime ( HAPITime _time ) {
+  playback_lock.lock();
+  seek_to_time= _time;
+  playback_lock.unlock();
+}
+
+void PlaybackHapticsDevice::setPlaybackSpeed ( HAPIFloat _speed ) {
+  playback_lock.lock();
+  playback_speed= _speed<0 ? 0 : _speed;
+  playback_lock.unlock();
+}
+
+HAPITime PlaybackHapticsDevice::getPlaybackTime () {
+  return playback_time;
+}
+
+bool PlaybackHapticsDevice::isPlaying () {
+  return playing;
+}
+
+bool PlaybackHapticsDevice::getPlaybackValuesNext ( HAPIHapticsDevice::DeviceValues& _dv, HAPITime& _time ) {
+  if ( binary ) {
+    return getPlaybackValuesNextBinary ( _dv, _time );
+  } else {
+    return getPlaybackValuesNextText ( _dv, _time );
+  }
+}
+
+bool PlaybackHapticsDevice::getPlaybackValuesNextText ( HAPIHapticsDevice::DeviceValues& _dv, HAPITime& _time ) {
+  if ( !playback_file.good() ) {
+    return false;
+  }
+
+  HAPIHapticsDevice::DeviceValues dv_tmp;
+  HAPITime time_tmp= -1;
+  for ( StringList::iterator i= field_names.begin(); i != field_names.end(); ++i ) {
+    std::string field_name= *i;
+
+    if ( field_name == "TIME" ) {
+      playback_file >> time_tmp;
+    }
+    else if ( field_name == "POSITION" ) {
+      HAPIFloat f;
+      playback_file >> f; playback_file >> f; playback_file >> f;
+    }
+    else if ( field_name == "ORIENTATION" ) {
+      HAPIFloat f;
+      playback_file >> f; playback_file >> f; playback_file >> f; playback_file >> f;
+    }
+    else if ( field_name == "VELOCITY" ) {
+      HAPIFloat f;
+      playback_file >> f; playback_file >> f; playback_file >> f;
+    }
+    else if ( field_name == "FORCE" ) {
+      HAPIFloat f;
+      playback_file >> f; playback_file >> f; playback_file >> f;
+    }
+    else if ( field_name == "TORQUE" ) {
+      HAPIFloat f;
+      playback_file >> f; playback_file >> f; playback_file >> f;
+    }
+    else if ( field_name == "BUTTONS" ) {
+      playback_file >> dv_tmp.button_status;
+    }
+    else if ( field_name == "RAW_POSITION" ) {
+      playback_file >> dv_tmp.position.x; playback_file >> dv_tmp.position.y; playback_file >> dv_tmp.position.z;
+    }
+    else if ( field_name == "RAW_ORIENTATION" ) {
+      playback_file >> dv_tmp.orientation.axis.x; playback_file >> dv_tmp.orientation.axis.y; playback_file >> dv_tmp.orientation.axis.z; playback_file >> dv_tmp.orientation.angle;
+    }
+    else if ( field_name == "RAW_VELOCITY" ) {
+      playback_file >> dv_tmp.velocity.x; playback_file >> dv_tmp.velocity.y; playback_file >> dv_tmp.velocity.z;
+    }
+    else if ( field_name == "RAW_FORCE" ) {
+      HAPIFloat f;
+      playback_file >> f; playback_file >> f; playback_file >> f;
+    }
+    else if ( field_name == "RAW_TORQUE" ) {
+      HAPIFloat f;
+      playback_file >> f; playback_file >> f; playback_file >> f;
+    }
+  }
+
+  if ( playback_file.good() ) {
+    _time= time_tmp;
+    _dv= dv_tmp;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool PlaybackHapticsDevice::getPlaybackValuesNextBinary ( HAPIHapticsDevice::DeviceValues& _dv, HAPITime& _time ) {
+  if ( !playback_file.good() ) {
+    return false;
+  }
+
+  HAPIHapticsDevice::DeviceValues dv_tmp;
+  HAPITime time_tmp= -1;
+  for ( StringList::iterator i= field_names.begin(); i != field_names.end(); ++i ) {
+    std::string field_name= *i;
+
+    if ( field_name == "TIME" ) {
+      H3DUtil::readH3DType ( playback_file, time_tmp );
+    }
+    else if ( field_name == "POSITION" ) {
+      HAPI::Vec3 p;
+      H3DUtil::readH3DType ( playback_file, p );
+    }
+    else if ( field_name == "ORIENTATION" ) {
+      HAPI::Rotation r;
+      H3DUtil::readH3DType ( playback_file, r );
+    }
+    else if ( field_name == "VELOCITY" ) {
+      HAPI::Vec3 v;
+      H3DUtil::readH3DType ( playback_file, v );
+    }
+    else if ( field_name == "FORCE" ) {
+      HAPI::Vec3 f;
+      H3DUtil::readH3DType ( playback_file, f );
+    }
+    else if ( field_name == "TORQUE" ) {
+      HAPI::Vec3 t;
+      H3DUtil::readH3DType ( playback_file, t );
+    }
+    else if ( field_name == "BUTTONS" ) {
+      H3DUtil::readH3DType ( playback_file, dv_tmp.button_status );
+    }
+    else if ( field_name == "RAW_POSITION" ) {
+      H3DUtil::readH3DType ( playback_file, dv_tmp.position );
+    }
+    else if ( field_name == "RAW_ORIENTATION" ) {
+      H3DUtil::readH3DType ( playback_file, dv_tmp.orientation );
+    }
+    else if ( field_name == "RAW_VELOCITY" ) {
+      H3DUtil::readH3DType ( playback_file, dv_tmp.velocity );
+    }
+    else if ( field_name == "RAW_FORCE" ) {
+      HAPI::Vec3 f;
+      H3DUtil::readH3DType ( playback_file, f );
+    }
+    else if ( field_name == "RAW_TORQUE" ) {
+      HAPI::Vec3 t;
+      H3DUtil::readH3DType ( playback_file, t );
+    }
+  }
+
+  if ( playback_file.good() ) {
+    _time= time_tmp;
+    _dv= dv_tmp;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+bool PlaybackHapticsDevice::getPlaybackValuesAtTime ( HAPI::HAPIHapticsDevice::DeviceValues &dv, HAPI::HAPITime _time ) {
+  if ( _time > playback_time ) {
+    bool success= getPlaybackValuesNext ( playback_device_values, playback_time );
+  
+    while ( success && playback_time < _time && playback_time > 0 ) {
+      success= getPlaybackValuesNext ( playback_device_values, playback_time );
+    }
+
+    dv= playback_device_values;
+    return success;
+  } else {
+    dv= playback_device_values;
+    return true;
+  }
+}
