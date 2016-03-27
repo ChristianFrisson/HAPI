433ca139904a311f5dce71e0465628aac22a2af9
daniel <daniel@f896c5a5-350d-0410-967e-bc877d8f5295>
2016-03-27 19:37:56 +0000

    UPDATE: added support for precompiled headers
    git-svn-id: https://www.h3dapi.org:8090/HAPI/trunk@818 f896c5a5-350d-0410-967e-bc877d8f5295

---
+++ include/HAPI/StdAfx.h
@@ -0,0 +1,63 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2013, SenseGraphics AB
+//
+//    Any use, or distribution, of this file without permission from the
+//    copyright holders is strictly prohibited. Please contact SenseGraphics,
+//    www.sensegraphics.com, for more information.
+//
+//
+/// \file StdAfx.h
+/// \brief Header file for precompiled headers.
+///
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef __H3D_HAPI_STDAFX__
+#define __H3D_HAPI_STDAFX__
+
+#include <GL/glew.h>
+#include <GL/gl.h> //3
+#include <GL/glu.h> //2
+#include <Windows.h>
+
+#include <H3DUtil/LinAlgTypes.h>
+#include <H3DUtil/TimeStamp.h>
+#include <H3DUtil/H3DMath.h>
+
+#include <H3DUtil/Console.h> //2
+#include <H3DUtil/DynamicLibrary.h> //8
+#include <H3DUtil/ExtremaFindingAlgorithms.h> //1
+#include <H3DUtil/H3DMath.h> //3
+#include <H3DUtil/H3DTimer.h> //1
+#include <H3DUtil/ReadWriteH3DTypes.h> //2
+#include <H3DUtil/Rotationd.h> //1
+#include <H3DUtil/AutoPtrVector.h> //1
+#include <H3DUtil/AutoRef.h> //3
+#include <H3DUtil/AutoRefVector.h> //5
+#include <H3DUtil/DynamicLibrary.h> //1
+#include <H3DUtil/H3DMath.h> //1
+#include <H3DUtil/Image.h> //2
+#include <H3DUtil/LinAlgTypes.h> //1
+#include <H3DUtil/RefCountedClass.h> //4
+#include <H3DUtil/Threads.h> //7
+#include <H3DUtil/TimeStamp.h> //2
+
+#include <algorithm> //2
+#include <cstdlib> //1
+#include <cstring> //2
+#include <dhdc.h> //1
+#include <fparser.hh> //1
+#include <fstream> //3
+#include <iostream> //1
+#include <limits> //1
+#include <map> //4
+#include <sstream> //5
+#include <stack> //1
+#include <stdlib.h> //1
+#include <string> //2
+
+#include <assert.h> //1
+#include <list> //2
+#include <memory> //3
+#include <pthread.h> //1
+
+#endif
---
+++ src/StdAfx.cpp
@@ -0,0 +1,1 @@
+#include <HAPI/StdAfx.h>
